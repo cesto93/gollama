e71bf462f532d0320806014f0813f1dfb6b9e26c
Jonathan Hecl <jonathanhecl@gmail.com>
2024-11-13 00:09:05 -0300

    TestGollama_Version

---
+++ v1/version_test.go
@@ -0,0 +1,31 @@
+package gollama
+
+import "testing"
+
+func TestGollama_Version(t *testing.T) {
+	tests := []struct {
+		name    string
+		c       *Gollama
+		want    string
+		wantErr bool
+	}{
+		{
+			name:    "Version",
+			c:       New("llama3.2"),
+			want:    "0.4.1",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.c.Version()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Gollama.Version() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Gollama.Version() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
