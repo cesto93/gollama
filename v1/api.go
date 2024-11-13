b1a4f82766732670a6a3ac3a2db3e26f385a7a57
Jonathan Hecl <jonathanhecl@gmail.com>
2024-11-13 00:08:25 -0300

    apiGet

--- v1/api.go
+++ v1/api.go
@@ -1,1 +1,24 @@
 package gollama
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+func (c *Gollama) apiGet(path string, v interface{}) error {
+	url, _ := url.JoinPath(c.ServerAddr, path)
+	if c.Verbose {
+		fmt.Printf("Sending a request to %s\n", url)
+	}
+	HTTPClient := &http.Client{
+		Timeout: c.HTTPTimeout,
+	}
+	resp, err := HTTPClient.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	return json.NewDecoder(resp.Body).Decode(v)
+}
