342c9b7a2faab5731042602b53e8971f78e15237
Jonathan Hecl <jonathanhecl@gmail.com>
2024-11-13 00:36:59 -0300

    apiPost

--- v1/api.go
+++ v1/api.go
@@ -1,6 +1,7 @@
 package gollama
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -20,5 +21,26 @@ func (c *Gollama) apiGet(path string, v interface{}) error {
 		return err
 	}
 	defer resp.Body.Close()
+	return json.NewDecoder(resp.Body).Decode(v)
+}
+
+func (c *Gollama) apiPost(path string, v interface{}, data interface{}) error {
+	url, _ := url.JoinPath(c.ServerAddr, path)
+	if c.Verbose {
+		fmt.Printf("Sending a request to %s\n", url)
+	}
+
+	reqBytes, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	HTTPClient := &http.Client{}
+	resp, err := HTTPClient.Post(url, "application/json", bytes.NewBuffer(reqBytes))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
 	return json.NewDecoder(resp.Body).Decode(v)
 }
