c6b28d05bd0edf24b34390522b27dc5c3de6f76e
Jonathan Hecl <jonathanhecl@gmail.com>
2024-12-14 01:09:12 -0300

    StructToStructuredFormat

---
+++ structs.go
@@ -0,0 +1,67 @@
+package gollama
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func StructToStructuredFormat(s interface{}) (StructuredFormat, error) {
+	structValue := reflect.ValueOf(s)
+	structType := structValue.Type()
+
+	properties := make(map[string]FormatProperty)
+
+	for i := 0; i < structType.NumField(); i++ {
+		field := structType.Field(i)
+
+		strType, strItems, err := fieldTypeToGollamaType(field.Type)
+		if err != nil {
+			return StructuredFormat{}, err
+		}
+
+		property := FormatProperty{
+			Type:        strType,
+			Description: field.Tag.Get("description"),
+		}
+
+		if strItems != "" {
+			property.Items = ItemProperty{
+				Type: strItems,
+			}
+		}
+
+		properties[field.Name] = property
+	}
+
+	return StructuredFormat{
+		Type:       "object",
+		Properties: properties,
+	}, nil
+
+}
+
+func fieldTypeToGollamaType(fieldType reflect.Type) (string, string, error) {
+	switch fieldType.Kind() {
+	case reflect.String:
+		return "string", "", nil
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return "integer", "", nil
+	case reflect.Float32, reflect.Float64:
+		return "number", "", nil
+	case reflect.Bool:
+		return "boolean", "", nil
+	default:
+		if fieldType == reflect.TypeOf([]int{}) || fieldType == reflect.TypeOf([]int8{}) ||
+			fieldType == reflect.TypeOf([]int16{}) || fieldType == reflect.TypeOf([]int32{}) ||
+			fieldType == reflect.TypeOf([]int64{}) {
+			return "array", "integer", nil
+		} else if fieldType == reflect.TypeOf([]float32{}) || fieldType == reflect.TypeOf([]float64{}) {
+			return "array", "number", nil
+		} else if fieldType == reflect.TypeOf([]string{}) {
+			return "array", "string", nil
+		} else if fieldType == reflect.TypeOf([]bool{}) {
+			return "array", "boolean", nil
+		}
+		return "string", "", fmt.Errorf("unsupported field type: %s", fieldType.String())
+	}
+}
---
+++ structs_test.go
@@ -0,0 +1,50 @@
+package gollama
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestStructToStructuredFormat(t *testing.T) {
+	type args struct {
+		s interface{}
+	}
+
+	type myStruct struct {
+		content string
+		value   bool  `description:"test value"`
+		list    []int `description:"test list"`
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		want    StructuredFormat
+		wantErr bool
+	}{
+		{
+			name: "StructToStructuredFormat",
+			args: args{s: myStruct{}},
+			want: StructuredFormat{Type: "object", Properties: map[string]FormatProperty{
+				"content": {Type: "string", Description: ""},
+				"value":   {Type: "boolean", Description: "test value"},
+				"list":    {Type: "array", Description: "test list", Items: ItemProperty{Type: "integer"}},
+			}},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := StructToStructuredFormat(tt.args.s)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("StructToStructuredFormat() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Logf("%+v", got)
+				t.Logf("%+v", tt.want)
+				t.Errorf("StructToStructuredFormat() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
